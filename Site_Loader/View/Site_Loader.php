aaca1522fa1383b3b38d32c87f50e3d4a8581d99
Oleksiy Boyda <oleksiyboyda@Oleksiys-MacBook-Pro.local>
2022-12-06 19:42:34 +0100

    updates

---
+++ Site_Loader/View/Site_Loader.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PBOOT\Mod\Site_Loader\View;
+
+class Site_Loader extends \PBOOT\View\View 
+{
+    public function __construct($args)
+    {
+        parent::__construct($args, [
+            
+        ]);
+    }
+}
