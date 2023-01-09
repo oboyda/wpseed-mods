c8f2ae9cc9f37f29c00370631836fff161654619
Oleksiy Boyda <oleksiyboyda@Oleksiys-MacBook-Pro.local>
2023-01-09 11:39:28 +0100

    updates

---
+++ Post_List/View/List_Filters_Hidden.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace WPSEEDM\Mod\Post_List\View;
+
+class List_Filters_Hidden extends Post_List_Filters_Form 
+{
+    public function __construct($args, $default_args=[])
+    {
+        parent::__construct($args, $default_args);
+    }
+}
