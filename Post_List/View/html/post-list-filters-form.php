70bc45e799941c3fec25282cfbae6aa7b2b2c842
Oleksiy Boyda <oleksiyboyda@Oleksiys-MacBook-Pro.local>
2023-01-23 20:48:47 +0100

    Post_List 1.0.35

--- Post_List/View/html/post-list-filters-form.php
+++ Post_List/View/html/post-list-filters-form.php
@@ -1,4 +1,4 @@
-<div class="<?php echo $view->getHtmlClass('d-none'); ?>" data="<?php echo $view->getName(); ?>">
+<div class="<?php echo $view->getHtmlClass('d-none'); ?>">
     <form class="ajax-form filters-form" action="" method="POST">
 
         <?php echo $view->printFormRequiredFields(); ?>
