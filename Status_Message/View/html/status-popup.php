d040f1ac89637d56772240ff4529b09f63387e8e
Oleksiy Boyda <oleksiyboyda@Oleksiys-MacBook-Pro.local>
2023-01-26 23:28:41 +0100

    Status_Message 1.2.1

--- Status_Message/View/html/status-popup.php
+++ Status_Message/View/html/status-popup.php
@@ -1,8 +1,6 @@
 <div class="<?php echo $view->getHtmlClass(); ?>" data-view="<?php echo $view->getName(); ?>">
     <div class="popup-inner">
         <span class="popup-close"><i class="bi bi-x"></i></span>
-        <div class="popup-message">
-            <p>Lorem ipsum dolor sin amet ipsum dolor sin amet ipsum dolor sin amet</p>
-        </div>
+        <div class="popup-message"></div>
     </div>
 </div>
