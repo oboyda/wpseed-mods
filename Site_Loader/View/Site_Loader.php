25304a9aa9da45fe36d9d3f52b5d858d9e741b0a
Oleksiy Boyda <oleksiyboyda@Oleksiys-MacBook-Pro.local>
2023-02-25 09:29:08 +0100

    fixes

--- Site_Loader/View/Site_Loader.php
+++ Site_Loader/View/Site_Loader.php
@@ -1,15 +1,30 @@
 <?php
 
-namespace WPSEEDM\Mod\Site_Loader\View;
+namespace DFRP\Mod\Site_Loader\View;
 
-class Site_Loader extends \WPSEEDM\View\View 
+class Site_Loader extends \DFRP\View\View 
 {
     CONST MOD_NAME = 'Site_Loader';
 
     public function __construct($args)
     {
         parent::__construct($args, [
-            
+            'pos' => '', #full
+            'shadow' => '' #light, dark
         ]);
+
+        $this->_setHtmlClass();
+    }
+
+    protected function _setHtmlClass()
+    {
+        if($this->args['pos'])
+        {
+            $this->addHtmlClass('pos-' . $this->args['pos']);
+        }
+        if($this->args['shadow'])
+        {
+            $this->addHtmlClass('shadow-' . $this->args['shadow']);
+        }
     }
 }
